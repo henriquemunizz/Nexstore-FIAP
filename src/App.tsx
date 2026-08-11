175443430594cf37a44a4dcde82ebc89ef5f0577
henriquemunizz <henriquedasilvamuniz03@gmail.com>
2026-08-11 20:03:29 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from './Button'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+
+      <Button/>
+
+    </div>
+  )
+}
+
+export default App
